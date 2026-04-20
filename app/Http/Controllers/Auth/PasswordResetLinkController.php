b5e566a8bc046c9ea3dd704290f3dddc7284e34d
reyon Lau <liurey55@gmail.com>
2026-04-20 14:07:55 +0700

    last update

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -29,10 +29,20 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
+        $isCustomer = \App\Models\Customer::where('email', $request->email)->exists();
+        $isAdmin = \App\Models\Admin::where('email', $request->email)->exists();
+
+        $broker = 'users';
+        if ($isCustomer) {
+            $broker = 'customers';
+        } elseif ($isAdmin) {
+            $broker = 'admins';
+        }
+
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
+        $status = Password::broker($broker)->sendResetLink(
             $request->only('email')
         );
 
